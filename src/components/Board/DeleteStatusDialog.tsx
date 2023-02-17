9a9dfaaa9920cce1b3f3a5c1bc77e827b83ceb3a
jakub-szewczyk <jakub.szewczyk.dev@gmail.com>
2023-02-17 18:21:42 +0100

    change delete status dialog confirmation message

--- src/components/Board/DeleteStatusDialog.tsx
+++ src/components/Board/DeleteStatusDialog.tsx
@@ -24,7 +24,7 @@ const DeleteStatusDialog = ({
     dialogTitle='Delete status'
     dialogContent={
       <Typography>
-        Are you sure you want to delete this status and all its related issues?
+        Are you sure you want to delete this status and all its issues?
       </Typography>
     }
     dialogActions={
