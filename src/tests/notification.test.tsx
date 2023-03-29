4f62ad3709803cddfa54b9866b00f74c2f2883d4
jakub-szewczyk <jakub.szewczyk.dev@gmail.com>
2023-03-29 22:39:38 +0200

    test notification module

--- src/tests/notification.test.tsx
+++ src/tests/notification.test.tsx
@@ -1,5 +1,12 @@
-import { fireEvent, render, screen, within } from '@testing-library/react'
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  within,
+} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { vi } from 'vitest'
 import App from '../App'
 
 beforeEach(() => localStorage.clear())
@@ -184,3 +191,191 @@ it('saves configuration via dialog', async () => {
     })
   )
 }, 45000)
+
+it('triggers at current time', async () => {
+  const NotificationMock = vi.fn()
+  // @ts-expect-error
+  NotificationMock.permission = 'granted'
+  vi.stubGlobal('Notification', NotificationMock)
+  vi.useFakeTimers()
+  vi.setSystemTime(new Date(1970, 1, 1, 8))
+  render(<App />)
+  const user = userEvent.setup({ delay: null })
+  await user.type(
+    within(
+      screen.getByRole('row', {
+        name: /monday/i,
+      })
+    ).getAllByRole('textbox')[0]!,
+    '0800a'
+  )
+  await user.click(screen.getAllByTestId('NotificationsNoneIcon')[0]!)
+  vi.advanceTimersByTime(1000)
+  fireEvent.contextMenu(
+    within(
+      within(
+        screen.getByRole('row', {
+          name: /monday/i,
+        })
+      ).getAllByRole('cell')[5]
+    ).getByRole('button')
+  )
+  await user.click(
+    await screen.findByRole('button', {
+      name: /current time/i,
+    })
+  )
+  await user.click(
+    screen.getByRole('option', {
+      name: '5 minutes before',
+    })
+  )
+  await user.click(
+    screen.getByRole('button', {
+      name: /save/i,
+    })
+  )
+  await user.click(screen.getByTestId('NotificationsIcon'))
+  await waitFor(() =>
+    fireEvent.contextMenu(screen.getAllByTestId('NotificationsNoneIcon')[0]!)
+  )
+  await user.click(
+    screen.getByRole('button', {
+      name: /5 minutes before/i,
+    })
+  )
+  await user.click(
+    screen.getByRole('option', {
+      name: /10 minutes before/i,
+    })
+  )
+  await user.click(
+    screen.getByRole('button', {
+      name: /save/i,
+    })
+  )
+  await waitFor(() =>
+    fireEvent.contextMenu(screen.getAllByTestId('NotificationsNoneIcon')[0]!)
+  )
+  await user.click(
+    screen.getByRole('button', {
+      name: /10 minutes before/i,
+    })
+  )
+  await user.click(
+    screen.getByRole('option', {
+      name: /15 minutes before/i,
+    })
+  )
+  await user.click(
+    screen.getByRole('button', {
+      name: /save/i,
+    })
+  )
+  await waitFor(() =>
+    fireEvent.contextMenu(screen.getAllByTestId('NotificationsNoneIcon')[0]!)
+  )
+  await user.click(
+    screen.getByRole('button', {
+      name: /15 minutes before/i,
+    })
+  )
+  await user.click(
+    screen.getByRole('option', {
+      name: /custom time/i,
+    })
+  )
+  await user.type(
+    screen.getByRole('textbox', {
+      name: /time/i,
+    }),
+    '{Backspace}{Backspace}{Backspace}{Backspace}{Backspace}{Delete}'
+  )
+  await user.type(
+    screen.getByRole('textbox', {
+      name: /time/i,
+    }),
+    '0740am'
+  )
+  await user.click(
+    screen.getByRole('button', {
+      name: /save/i,
+    })
+  )
+  await waitFor(() =>
+    fireEvent.contextMenu(screen.getAllByTestId('NotificationsNoneIcon')[0]!)
+  )
+  vi.useRealTimers()
+  vi.unstubAllGlobals()
+}, 20000)
+
+it('triggers 5 minutes before', async () => {
+  const NotificationMock = vi.fn()
+  // @ts-expect-error
+  NotificationMock.permission = 'granted'
+  vi.stubGlobal('Notification', NotificationMock)
+  vi.useFakeTimers()
+  vi.setSystemTime(new Date(1970, 1, 1, 7, 55))
+  render(<App />)
+  const user = userEvent.setup({ delay: null })
+  await user.type(
+    within(
+      screen.getByRole('row', {
+        name: /monday/i,
+      })
+    ).getAllByRole('textbox')[0]!,
+    '0755a'
+  )
+  await user.click(screen.getAllByTestId('NotificationsNoneIcon')[0]!)
+  vi.advanceTimersByTime(1000)
+  vi.useRealTimers()
+  vi.unstubAllGlobals()
+}, 15000)
+
+it('triggers 10 minutes before', async () => {
+  const NotificationMock = vi.fn()
+  // @ts-expect-error
+  NotificationMock.permission = 'granted'
+  vi.stubGlobal('Notification', NotificationMock)
+  vi.useFakeTimers()
+  vi.setSystemTime(new Date(1970, 1, 1, 7, 50))
+  render(<App />)
+  const user = userEvent.setup({ delay: null })
+  await user.type(
+    within(
+      screen.getByRole('row', {
+        name: /monday/i,
+      })
+    ).getAllByRole('textbox')[0]!,
+    '0750a'
+  )
+  await user.click(screen.getAllByTestId('NotificationsNoneIcon')[0]!)
+  vi.advanceTimersByTime(1000)
+  vi.useRealTimers()
+  vi.unstubAllGlobals()
+}, 15000)
+
+it('triggers 15 minutes before', async () => {
+  const NotificationMock = vi.fn()
+  // @ts-expect-error
+  NotificationMock.permission = 'denied'
+  // @ts-expect-error
+  NotificationMock.requestPermission = vi.fn(() => Promise.resolve())
+  vi.stubGlobal('Notification', NotificationMock)
+  vi.useFakeTimers()
+  vi.setSystemTime(new Date(1970, 1, 1, 7, 45))
+  render(<App />)
+  const user = userEvent.setup({ delay: null })
+  await user.type(
+    within(
+      screen.getByRole('row', {
+        name: /monday/i,
+      })
+    ).getAllByRole('textbox')[0]!,
+    '0745a'
+  )
+  await user.click(screen.getAllByTestId('NotificationsNoneIcon')[0]!)
+  vi.advanceTimersByTime(1000)
+  vi.useRealTimers()
+  vi.unstubAllGlobals()
+}, 15000)
