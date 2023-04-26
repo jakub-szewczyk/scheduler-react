924ee781b9f394efc8c39ccabfe4d911aa5c56c8
jakub-szewczyk <jakub.szewczyk.dev@gmail.com>
2023-04-26 20:40:58 +0200

    reuse notes hook

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,79 +1,65 @@
+import useNotes from '@/hooks/useNotes'
 import { isPlaceholderVisible } from '@/modules/note'
 import { DraftHandleValue, Editor, EditorState, RichUtils } from 'draft-js'
 import 'draft-js/dist/Draft.css'
-import {
-  Dispatch,
-  RefObject,
-  SetStateAction,
-  forwardRef,
-  useState,
-} from 'react'
+import { RefObject, forwardRef, useState } from 'react'
+import NoteHeader from './NoteHeader'
 import Toolbar from './Toolbar'
 import { EditorContainer, NoteContainer } from './styles/Note.styled'
-import { Note as INote } from '@/types/note'
-import NoteHeader from './NoteHeader'
 
-interface NoteProps {
-  note: INote
-  notes: INote[]
-  setNotes: Dispatch<SetStateAction<INote[]>>
-  editorState: EditorState
-  setEditorState: Dispatch<SetStateAction<EditorState>>
-}
+const Note = forwardRef<Editor>((_, ref) => {
+  const editorRef = ref as RefObject<Editor>
 
-const Note = forwardRef<Editor, NoteProps>(
-  ({ note, notes, setNotes, editorState, setEditorState }, ref) => {
-    const editorRef = ref as RefObject<Editor>
+  const { note, notes, setNotes, editorState, setEditorState } = useNotes()
 
-    const [spellCheck, setSpellCheck] = useState(true)
+  const [spellCheck, setSpellCheck] = useState(true)
 
-    const handleKeyCommand = (
-      command: string,
-      editorState: EditorState,
-      eventTimeStamp: number
-    ): DraftHandleValue => {
-      const newEditorState = RichUtils.handleKeyCommand(editorState, command)
-      if (newEditorState) {
-        setEditorState(newEditorState)
-        return 'handled'
-      }
-      return 'not-handled'
+  const handleKeyCommand = (
+    command: string,
+    editorState: EditorState,
+    eventTimeStamp: number
+  ): DraftHandleValue => {
+    const newEditorState = RichUtils.handleKeyCommand(editorState, command)
+    if (newEditorState) {
+      setEditorState(newEditorState)
+      return 'handled'
     }
-
-    return (
-      <>
-        <NoteHeader note={note} notes={notes} setNotes={setNotes} />
-        <NoteContainer>
-          <Toolbar
-            ref={editorRef}
-            note={note}
-            editorState={editorState}
-            setEditorState={setEditorState}
-            spellCheck={spellCheck}
-            setSpellCheck={setSpellCheck}
-          />
-          <EditorContainer
-            elevation={0}
-            onClick={() => editorRef.current?.focus()}
-            sx={{
-              '.public-DraftEditorPlaceholder-root': {
-                display: isPlaceholderVisible(editorState) ? 'block' : 'none',
-              },
-            }}
-          >
-            <Editor
-              ref={editorRef}
-              editorState={editorState}
-              placeholder='Enter your note here...'
-              spellCheck={spellCheck}
-              onChange={setEditorState}
-              handleKeyCommand={handleKeyCommand}
-            />
-          </EditorContainer>
-        </NoteContainer>
-      </>
-    )
+    return 'not-handled'
   }
-)
+
+  return (
+    <>
+      <NoteHeader note={note} notes={notes} setNotes={setNotes} />
+      <NoteContainer>
+        <Toolbar
+          ref={editorRef}
+          note={note}
+          editorState={editorState}
+          setEditorState={setEditorState}
+          spellCheck={spellCheck}
+          setSpellCheck={setSpellCheck}
+        />
+        <EditorContainer
+          elevation={0}
+          onClick={() => editorRef.current?.focus()}
+          sx={{
+            '.public-DraftEditorPlaceholder-root': {
+              display: isPlaceholderVisible(editorState) ? 'block' : 'none',
+            },
+          }}
+        >
+          <Editor
+            ref={editorRef}
+            editorState={editorState}
+            placeholder='Enter your note here...'
+            spellCheck={spellCheck}
+            onChange={setEditorState}
+            handleKeyCommand={handleKeyCommand}
+          />
+        </EditorContainer>
+      </NoteContainer>
+    </>
+  )
+})
 
 export default Note
--- src/pages/Notes/Notes.tsx
+++ src/pages/Notes/Notes.tsx
@@ -1,18 +1,15 @@
 import Note from '@/components/Note/Note'
 import NoteActions from '@/components/NoteActions/NoteActions'
-import useNotes from '@/hooks/useNotes'
 import { Editor } from 'draft-js'
 import { useRef } from 'react'
 
 const Notes = () => {
   const editorRef = useRef<Editor>(null)
 
-  const notes = useNotes()
-
   return (
     <>
-      <Note ref={editorRef} {...notes} />
-      <NoteActions ref={editorRef} {...notes} />
+      <Note ref={editorRef} />
+      <NoteActions ref={editorRef} />
     </>
   )
 }
