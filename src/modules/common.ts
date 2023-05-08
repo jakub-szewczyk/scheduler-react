942e940201df1490295c610d84f58eacad326f04
jakub-szewczyk <jakub.szewczyk.dev@gmail.com>
2023-05-08 19:30:19 +0200

    update common module exports

--- src/modules/common.ts
+++ src/modules/common.ts
@@ -6,11 +6,7 @@ import { Schedule } from '../types/schedule'
 import { Note } from '@/types/note'
 import { Project } from '@/types/project'
 
-const isUnsaved: (widget: Project | Schedule | Board | Note) => boolean = flow(
-  prop('name'),
-  equals('unsaved')
-)
+export const isUnsaved: (widget: Project | Schedule | Board | Note) => boolean =
+  flow(prop('name'), equals('unsaved'))
 
-const asteriskSuffix = when(equals('unsaved'), concat(__, '*'))
-
-export { isUnsaved, asteriskSuffix }
+export const asteriskSuffix = when(equals('unsaved'), concat(__, '*'))
