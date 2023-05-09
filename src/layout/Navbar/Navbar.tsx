88cf9480a8d8b9edee30f4e71c155ecdbce1aa66
jakub-szewczyk <jakub.szewczyk.dev@gmail.com>
2023-05-09 17:34:07 +0200

    handle index page navigation

--- src/layout/Navbar/Navbar.tsx
+++ src/layout/Navbar/Navbar.tsx
@@ -1,23 +1,43 @@
 import PendingActionsIcon from '@mui/icons-material/PendingActions'
 import { AppBar, Box, Stack, Toolbar, Typography } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 import WidgetsMenu from './WidgetsMenu'
 
-const Navbar = () => (
-  <Box sx={{ flexGrow: 1 }}>
-    <AppBar elevation={0}>
-      <Toolbar sx={{ justifyContent: 'space-between' }}>
-        <Stack direction='row' alignItems='center' columnGap={1.5}>
-          <PendingActionsIcon />
-          <Typography component='div' variant='h6' sx={{ flexGrow: 1 }}>
-            Scheduler
-          </Typography>
-        </Stack>
-        <Stack>
-          <WidgetsMenu />
-        </Stack>
-      </Toolbar>
-    </AppBar>
-  </Box>
-)
+const Navbar = () => {
+  const navigate = useNavigate()
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar elevation={0}>
+        <Toolbar
+          sx={{
+            justifyContent: 'space-between',
+            height: {
+              xs: '56px',
+              sm: '64px',
+            },
+          }}
+        >
+          <Stack
+            direction='row'
+            alignItems='center'
+            columnGap={1.5}
+            height='100%'
+            onClick={() => navigate('/')}
+            sx={{ cursor: 'pointer' }}
+          >
+            <PendingActionsIcon />
+            <Typography component='div' variant='h6' sx={{ flexGrow: 1 }}>
+              Scheduler
+            </Typography>
+          </Stack>
+          <Stack>
+            <WidgetsMenu />
+          </Stack>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
 
 export default Navbar
