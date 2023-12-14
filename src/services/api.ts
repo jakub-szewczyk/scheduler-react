e41c4d31e94562551da796afc5da8415b7b36276
jakub-szewczyk <jakub.szewczyk.dev@gmail.com>
2023-12-14 19:59:07 +0100

    update api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,7 @@
 import axios from 'axios'
 
-/**
- * TODO:
- * Save `baseURL` in .env file.
- */
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_BASE_API_URL,
 })
 
 export default api
