179ec93196d89ac1f4d6e2bda562da7b738f99b8
jakub-szewczyk <jakub.szewczyk.dev@gmail.com>
2024-05-23 19:50:34 +0200

    feat: add tooltip component

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -4,13 +4,13 @@ import SidebarLink from '../SidebarLink/SidebarLink'
 const Sidebar = () => (
   <aside className='w-14 border-r h-[calc(100vh-3rem)]'>
     <nav className='flex flex-col items-center gap-y-2 h-full py-2'>
-      <SidebarLink to='/'>
+      <SidebarLink to='/' tooltip='Dashboard'>
         <Home />
       </SidebarLink>
-      <SidebarLink to='/projects'>
+      <SidebarLink to='/projects' tooltip='Projects'>
         <Folder />
       </SidebarLink>
-      <SidebarLink className='mt-auto' to='/settings'>
+      <SidebarLink className='mt-auto' to='/settings' tooltip='Settings'>
         <Settings />
       </SidebarLink>
     </nav>
--- src/components/layout/SidebarLink/SidebarLink.tsx
+++ src/components/layout/SidebarLink/SidebarLink.tsx
@@ -1,22 +1,42 @@
 import { Button, ButtonProps } from '@/components/ui/button'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
 import { Link, LinkProps } from '@tanstack/react-router'
+import { ReactNode } from 'react'
 
 interface SidebarLink extends LinkProps {
   className?: ButtonProps['className']
+  tooltip?: ReactNode
 }
 
-const SidebarLink = ({ children, className, ...props }: SidebarLink) => (
-  <Button className={className} asChild variant='ghost' size='icon'>
-    <Link
-      inactiveProps={{
-        className:
-          'text-muted-foreground transition-colors hover:text-foreground',
-      }}
-      {...props}
-    >
-      {children}
-    </Link>
-  </Button>
+const SidebarLink = ({
+  children,
+  className,
+  tooltip,
+  ...props
+}: SidebarLink) => (
+  <TooltipProvider delayDuration={100}>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button className={className} asChild variant='ghost' size='icon'>
+          <Link
+            inactiveProps={{
+              className:
+                'text-muted-foreground transition-colors hover:text-foreground',
+            }}
+            {...props}
+          >
+            {children}
+          </Link>
+        </Button>
+      </TooltipTrigger>
+      {tooltip && <TooltipContent side='right'>{tooltip}</TooltipContent>}
+    </Tooltip>
+  </TooltipProvider>
 )
 
 export default SidebarLink
