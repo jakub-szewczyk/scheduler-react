7e6231691de791dca044abe09a318dd388cb21d5
jakub-szewczyk <jakub.szewczyk.dev@gmail.com>
2024-06-02 11:09:22 +0200

    docs: add sidebar stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/react'
+import { withRouter } from '../src/utils/storybook'
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +10,7 @@ const preview: Preview = {
       },
     },
   },
+  decorators: withRouter,
 }
 
 export default preview
--- src/components/common/Preloader/Preloader.tsx
+++
@@ -1,9 +0,0 @@
-import { LoaderCircle } from 'lucide-react'
-
-const Preloader = () => (
-  <div className='flex items-center justify-center w-screen h-screen'>
-    <LoaderCircle className='animate-spin' />
-  </div>
-)
-
-export default Preloader
