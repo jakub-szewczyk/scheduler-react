1b632ec6ef6e987b0b345e953c0de0f39e2bae79
jakub-szewczyk <jakub.szewczyk.dev@gmail.com>
2024-06-05 20:49:26 +0200

    docs: add delete confirmation dialog story

--- src/components/common/ActionsDropdown/ActionsDropdown.stories.tsx
+++ src/components/common/ActionsDropdown/ActionsDropdown.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { FileText, Pencil, Trash } from 'lucide-react'
 import '../../../index.css'
 import ActionsDropdown from './ActionsDropdown'
 
@@ -13,6 +14,32 @@ type Story = StoryObj<typeof ActionsDropdown>
 
 export const Primary: Story = {
   args: {
+    items: [
+      {
+        children: (
+          <div className='flex items-center justify-center gap-x-2'>
+            <FileText className='w-4 h-4' />
+            Details
+          </div>
+        ),
+      },
+      {
+        children: (
+          <div className='flex items-center justify-center gap-x-2'>
+            <Pencil className='w-4 h-4' />
+            Edit
+          </div>
+        ),
+      },
+      {
+        children: (
+          <div className='flex items-center justify-center gap-x-2 text-destructive'>
+            <Trash className='w-4 h-4' />
+            Delete
+          </div>
+        ),
+      },
+    ],
     buttonProps: { disabled: false },
     dropdownMenuContentProps: { align: 'start' },
   },
