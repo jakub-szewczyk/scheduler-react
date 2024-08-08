72cfd6c516e28bdbc8668b6a985f97469233bbd3
jakub-szewczyk <jakub.szewczyk.dev@gmail.com>
2024-08-08 18:31:08 +0200

    feat: improve sidebar transitions

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -5,11 +5,31 @@ import {
   Folder,
   Home,
   ListCollapse,
+  LucideProps,
   Settings,
 } from 'lucide-react'
-import { Dispatch, SetStateAction } from 'react'
+import {
+  Dispatch,
+  ForwardRefExoticComponent,
+  ReactNode,
+  RefAttributes,
+  SetStateAction,
+  createElement,
+} from 'react'
 import SidebarButton from './SidebarButton/SidebarButton'
 import SidebarLink from './SidebarLink/SidebarLink'
+
+const SIDEBAR_LINKS: {
+  to: SidebarLink['to']
+  title: ReactNode
+  icon: ForwardRefExoticComponent<
+    Omit<LucideProps, 'ref'> & RefAttributes<SVGSVGElement>
+  >
+}[] = [
+  { to: '/', title: 'Dashboard', icon: Home },
+  { to: '/projects', title: 'Projects', icon: Folder },
+  { to: '/settings', title: 'Settings', icon: Settings },
+]
 
 interface SidebarProps {
   isCollapsed: boolean
@@ -19,93 +39,61 @@ interface SidebarProps {
 const Sidebar = ({ isCollapsed, setIsCollapsed }: SidebarProps) => (
   <aside
     className={cn(
-      'z-10 fixed inset-y-0 w-14 border-r bg-background transition-all duration-200',
+      'fixed inset-y-0 z-10 w-14 border-r bg-background transition-[width] duration-300',
       !isCollapsed && 'w-52'
     )}
   >
-    <div
-      className={cn(
-        'flex w-full h-12 items-center justify-center gap-x-2',
-        !isCollapsed && 'justify-start pl-4'
-      )}
-    >
-      {!isCollapsed ? (
-        <Link
-          to='/'
-          className='flex items-center justify-center gap-x-2 transition-transform duration-100 animate-[fade-in_300ms] hover:scale-[1.025]'
-        >
-          <div className='w-fit p-1.5 rounded-full bg-foreground text-background animate-[fade-in_300ms]'>
-            <CalendarCheck size='20' />
-          </div>
-          <p className='font-bold text-lg animate-[fade-in_300ms]'>Scheduler</p>
-        </Link>
-      ) : (
-        <Link
-          to='/'
-          className='w-fit p-1.5 rounded-full bg-foreground text-background transition-transform duration-100 animate-[fade-in_300ms] hover:scale-105'
-        >
+    <div className={cn('flex h-12 w-full items-center justify-center gap-x-2')}>
+      <Link
+        to='/'
+        className='flex w-full items-center gap-x-2 pl-3 transition-transform duration-100 hover:scale-[1.025]'
+      >
+        <div className='w-fit rounded-full bg-foreground p-1.5 text-background'>
           <CalendarCheck size='20' />
-        </Link>
-      )}
+        </div>
+        <p
+          className={cn('truncate text-lg font-bold', isCollapsed && 'hidden')}
+        >
+          Scheduler
+        </p>
+      </Link>
     </div>
     <nav
       className={cn(
-        'absolute left-0 bottom-0 flex flex-col items-center gap-y-2 w-full h-[calc(100vh-3rem+1px)] py-2 border-t sm:py-4',
+        'absolute bottom-0 left-0 flex h-[calc(100vh-3rem+1px)] w-full flex-col items-center gap-y-2 border-t py-2 sm:py-4',
         !isCollapsed && 'items-start'
       )}
     >
-      <SidebarLink
-        className={cn(!isCollapsed && 'w-full pl-4')}
-        to='/'
-        tooltip={isCollapsed && 'Dashboard'}
-      >
-        {!isCollapsed ? (
-          <div className='flex items-center gap-x-2 w-full animate-[fade-in_300ms]'>
-            <Home /> <span>Dashboard</span>
-          </div>
-        ) : (
-          <Home className='animate-[fade-in_300ms]' />
-        )}
-      </SidebarLink>
-      <SidebarLink
-        className={cn(!isCollapsed && 'w-full pl-4')}
-        to='/projects'
-        tooltip={isCollapsed && 'Projects'}
-      >
-        {!isCollapsed ? (
-          <div className='flex items-center gap-x-2 w-full animate-[fade-in_300ms]'>
-            <Folder /> <span>Projects</span>
-          </div>
-        ) : (
-          <Folder className='animate-[fade-in_300ms]' />
-        )}
-      </SidebarLink>
-      <SidebarLink
-        className={cn('mt-auto', !isCollapsed && 'w-full pl-4')}
-        to='/settings'
-        tooltip={isCollapsed && 'Settings'}
-      >
-        {!isCollapsed ? (
-          <div className='flex items-center gap-x-2 w-full animate-[fade-in_300ms]'>
-            <Settings /> <span>Settings</span>
-          </div>
-        ) : (
-          <Settings className='animate-[fade-in_300ms]' />
-        )}
-      </SidebarLink>
+      {SIDEBAR_LINKS.map(({ to, title, icon }) => (
+        <SidebarLink
+          key={to}
+          className={cn(
+            'mx-auto flex w-[calc(100%-1rem)] justify-start gap-x-2 px-2',
+            to === '/settings' && 'mt-auto'
+          )}
+          to={to}
+          tooltip={isCollapsed && title}
+        >
+          {createElement(icon, { className: 'flex-shrink-0' })}
+          <span className={cn('truncate', isCollapsed && 'hidden')}>
+            {title}
+          </span>
+        </SidebarLink>
+      ))}
       <SidebarButton
-        className={cn(!isCollapsed && 'w-full pl-4')}
+        className='mx-auto flex w-[calc(100%-1rem)] justify-start gap-x-2 px-2'
         tooltip={isCollapsed && 'Expand'}
         onClick={() => setIsCollapsed((isCollapsed) => !isCollapsed)}
       >
-        {!isCollapsed ? (
-          <div className='flex items-center gap-x-2 w-full animate-[fade-in_300ms]'>
-            <ListCollapse className='[transform:rotateY(180deg)]' />
-            <span>Collapse</span>
-          </div>
-        ) : (
-          <ListCollapse className='animate-[fade-in_300ms]' />
-        )}
+        <ListCollapse
+          className={cn(
+            'flex-shrink-0',
+            !isCollapsed && '[transform:rotateY(180deg)]'
+          )}
+        />
+        <span className={cn('truncate', isCollapsed && 'hidden')}>
+          {isCollapsed ? 'Expand' : 'Collapse'}
+        </span>
       </SidebarButton>
     </nav>
   </aside>
